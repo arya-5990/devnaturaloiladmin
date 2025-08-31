4afa63782a8144906bc8519fc84556f65725ed6c
ARYA SHARMA <aryasha4906c@gmail.com>
2025-08-31 10:34:08 +0530

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -13,10 +13,10 @@ const Sidebar = () => {
   const navItems = [
     { path: '/products', label: 'Products', icon: <FaBox /> },
     // { path: '/add-product', label: 'Add Product', icon: <FaPlus /> },
-    { path: '/add-blog', label: 'Add Blog', icon: <FaBlog /> },
-    { path: '/add-category', label: 'Add Category', icon: <FaTags /> },
-    { path: '/add-banner', label: 'Add Banner', icon: <FaImages /> },
-    { path: '/manage-users', label: 'Manage Users', icon: <FaUsers /> },
+    //{ path: '/add-blog', label: 'Add Blog', icon: <FaBlog /> },
+    //{ path: '/add-category', label: 'Add Category', icon: <FaTags /> },
+    //{ path: '/add-banner', label: 'Add Banner', icon: <FaImages /> },
+    //{ path: '/manage-users', label: 'Manage Users', icon: <FaUsers /> },
   ];
 
   return (
